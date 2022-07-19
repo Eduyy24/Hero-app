aaa7ce7916376ede0f9302730551d40696fb4e94
Ing. Eduardo Pinedo <33038866+Eduyy24@users.noreply.github.com>
2022-07-19 11:12:22 -0500

    add persistor config

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import { BrowserRouter } from "react-router-dom";
-import store from './redux/store';
-
-
+import {persistor, store} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk';
+import storage from 'redux-persist/lib/storage'
+
+import { persistReducer, persistStore } from 'redux-persist';
 import generalReducer from './slices/generalSlice'
 
-export default configureStore({
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, generalReducer)
+
+const store = configureStore({
   reducer: {
-    general: generalReducer
+    general: persistedReducer
   },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware:[thunk]
 })
+
+const persistor = persistStore(store);
+
+export { store, persistor };
