c70a3763afb7513d1f63acb5344a9409e814768e
Ing. Eduardo Pinedo <33038866+Eduyy24@users.noreply.github.com>
2022-07-20 09:04:01 -0500

    progress bar component implement

---
+++ src/components/progress-bar/ProgressBar.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+import styles from './ProgressBar.module.css'
+type Props = {
+  max:number;
+  value: number;
+}
+export const ProgressBar: FunctionComponent<Props> = ({max, value}) => {
+  return (
+    <div>
+      <progress className={styles.progressBar} value={value} max={max} />
+      <p className={styles.count}>{value} / {max}</p>
+    </div>
+  )
+
+}
--- src/pages/home/components/steps/Steps.tsx
+++ src/pages/home/components/steps/Steps.tsx
@@ -1,10 +1,10 @@
+import { ProgressBar } from '../../../../components/progress-bar/ProgressBar'
 import { WrapperSteps } from '../../../../components/wrapper-steps/WrapperSteps'
-import styles from './Steps.module.css'
 
 export const Steps = () => {
   return (
     <WrapperSteps>
-      <progress value={50} max={100} />
+      <ProgressBar value={90} max={100}/>
     </WrapperSteps>
   )
 }
