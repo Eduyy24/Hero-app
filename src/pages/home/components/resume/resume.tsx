29578630ec6d08483c5628542f4779404e557848
Ing. Eduardo Pinedo <33038866+Eduyy24@users.noreply.github.com>
2022-07-21 09:06:38 -0500

    clear data  and navigate in finalize resume

--- src/pages/home/components/resume/resume.tsx
+++ src/pages/home/components/resume/resume.tsx
@@ -1,74 +1,83 @@
-import { useSelector } from "react-redux"
-import { GeneralButton } from "../../../../components/general-button/GeneralButton"
-import { WrapperSteps } from "../../../../components/wrapper-steps/WrapperSteps"
-import usePagesData from "../../../../hooks/usePagesData"
-import { GeneralState } from "../../../../redux/slices/generalSlice"
-import { formatMoney } from "../../../../utils/utils"
-import styles from './Resume.module.css'
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { GeneralButton } from "../../../../components/general-button/GeneralButton";
+import { WrapperSteps } from "../../../../components/wrapper-steps/WrapperSteps";
+import usePagesData from "../../../../hooks/usePagesData";
+import { clearDataSell, GeneralState } from "../../../../redux/slices/generalSlice";
+import { formatMoney } from "../../../../utils/utils";
+import styles from "./Resume.module.css";
 
 type DataProcess = {
   key: string;
   value: string;
-}
+};
 
 export const Resume = () => {
-  const dataSell = useSelector((state: {general: GeneralState}) => state.general.dataSell)
-  const {getFieldForKey} = usePagesData()
-  const dataResume = Object.entries(dataSell)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const dataSell = useSelector(
+    (state: { general: GeneralState }) => state.general.dataSell
+  );
+  const { getFieldForKey } = usePagesData();
+  const dataResume = Object.entries(dataSell);
 
   const processData = () => {
-    const dataArr:DataProcess[] = [];
+    const dataArr: DataProcess[] = [];
     for (const data of dataResume) {
       const field = getFieldForKey(data[0]);
-      if(field) {
-        let value = ''
+      if (field) {
+        let value = "";
 
-        if(field.type === 'string') {
+        if (field.type === "string") {
           value = data[1] as string;
           dataArr.push({
             key: field.label,
-            value
-          })
-        };
+            value,
+          });
+        }
 
-        if(field.type === 'money') {
+        if (field.type === "money") {
           value = formatMoney(data[1] as string);
           dataArr.push({
             key: field.label,
-            value
-          })
-        };
+            value,
+          });
+        }
 
-        if(field.type === 'bool') {
-          value = (data[1] as boolean) ? 'SI' : 'NO';
+        if (field.type === "bool") {
+          value = (data[1] as boolean) ? "SI" : "NO";
           dataArr.push({
             key: field.label,
-            value
-          })
-        };
+            value,
+          });
+        }
       }
     }
     return dataArr;
-  }
+  };
+
+  const onClickFinalize = () => {
+    alert("Proceso finalizado");
+    dispatch(clearDataSell())
+    navigate('/')
+  };
 
   return (
     <WrapperSteps>
       <>
         <h3 className={styles.title}>Resumen de venta</h3>
-        {
-          processData().map((data, idx) => {
-            return (
-              <div className={styles.row} key={idx}>
-                <strong>{data.key}:</strong>
-                <span>{data.value}</span>
-              </div>
-            )
-          })
-        }
+        {processData().map((data, idx) => {
+          return (
+            <div className={styles.row} key={idx}>
+              <strong>{data.key}:</strong>
+              <span>{data.value}</span>
+            </div>
+          );
+        })}
         <div className={styles.containerButton}>
-          <GeneralButton name="Finalizar" onClick={()=> {alert('Proceso finalizado')}}/>
+          <GeneralButton name="Finalizar" onClick={onClickFinalize} />
         </div>
       </>
     </WrapperSteps>
   );
-}
+};
